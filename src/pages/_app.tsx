1e7d80b5df88655ade00bdfaac4030fdd14144aa
vilsonfcastilho <vilsonfcastilho@gmail.com>
2021-07-05 19:01:01 -0300

    create next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import { Provider as NextAuthProvider } from 'next-auth/client';
 
 import { Header } from '../components/Header';
 
@@ -6,9 +7,9 @@ import '../styles/global.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   );
 };
